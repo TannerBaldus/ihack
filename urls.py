3d57a426ae9b95fa8b0aac6c9e529af30ecb430b
Tanner Baldus <t.baldus285@gmail.com>
2014-08-09 13:20:17 -0700

    Started new virtualenv and alton project

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import patterns, url
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
-urlpatterns = patterns('',
-	url(r'', 'foodly.views.main', name='main'),
-)
